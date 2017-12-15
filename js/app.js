4463979d37ced32bd1169bfefe31e1a277aa5ac9
pawanpatil08 <pwnpatil08@gmail.com>
2017-12-15 21:31:33 +0530

    Hi

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 var app = angular.module('smartClassApp', []);
-			app.controller('smartClassCtrl', function($scope) {
+			app.controller('smartClassCtrl', function($scope,$http) {
 			$scope.carouselExampleIndicators = true;
-			$scope.showCarousel= function(){
-				$scope.carouselExampleIndicators = true;
+
+			$scope.loadCourseData= function(CourseTemplate){
+				switch(CourseTemplate){
+				case "firstYear":
+					$scope.showFirstCourse = true;
+					$scope.carouselExampleIndicators = false;
+
+				case "Gate":
+					$scope.showGateCourse = true;
+					$scope.carouselExampleIndicators = false;
+
+				case "GRE":
+					$scope.showGRECourse = true;
+					$scope.carouselExampleIndicators = false;	
+				}
+				
 			}
 			$scope.hideCarousel= function(){
 				$scope.carouselExampleIndicators = false;
 				$scope.showCourse = true;
 			}
 				
-			});
+});
