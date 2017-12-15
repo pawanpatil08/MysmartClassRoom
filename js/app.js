a991a9e4235cca0237ef38006fa90b6959fb025b
pawanpatil08 <pwnpatil08@gmail.com>
2017-12-15 23:58:11 +0530

    Last Commit For Today

--- js/app.js
+++ js/app.js
@@ -1,26 +1,51 @@
 var app = angular.module('smartClassApp', []);
 			app.controller('smartClassCtrl', function($scope,$http) {
 			$scope.carouselExampleIndicators = true;
-
+			$scope.HomeCarousel=true;
 			$scope.loadCourseData= function(CourseTemplate){
 				switch(CourseTemplate){
 				case "firstYear":
+					closeAllTemplate();
 					$scope.showFirstCourse = true;
-					$scope.carouselExampleIndicators = false;
-
+				
+					break;
 				case "Gate":
+					closeAllTemplate();
 					$scope.showGateCourse = true;
-					$scope.carouselExampleIndicators = false;
-
+				
+					break;
 				case "GRE":
+					closeAllTemplate();
 					$scope.showGRECourse = true;
-					$scope.carouselExampleIndicators = false;	
+					break;
+				case "home":
+					closeAllTemplate();
+					$scope.carouselExampleIndicators = true;
+					$scope.HomeCarousel=true;
+					break;
+
+					case "IIT-JEE":
+					closeAllTemplate();
+					$scope.showJEECourse=true;
+					break;
+					
+					case "NEET":
+					closeAllTemplate();
+					$scope.showNEETCourse=true;
+					break;
+
+					default:
 				}
 				
 			}
-			$scope.hideCarousel= function(){
-				$scope.carouselExampleIndicators = false;
-				$scope.showCourse = true;
+			function closeAllTemplate(){
+					$scope.showGateCourse = false;
+					$scope.showFirstCourse = false;
+					$scope.showGRECourse = false;
+					$scope.carouselExampleIndicators = false;
+					$scope.HomeCarousel=false;
+					$scope.showJEECourse=false;
+					$scope.showNEETCourse=false;
 			}
-				
+			 
 });
